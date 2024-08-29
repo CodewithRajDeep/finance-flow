e7379a2d366b388f1308a5edb9c8a7e6c5aab8e2
CodewithRajDeep <deepraj6897@gmail.com>
2024-08-29 17:43:52 +0530

    Implement Bank Details and Card UI with home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/ui/MobileNav";
 import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -11,7 +13,15 @@ export default function RootLayout({
   return (
  <main className="flex h-screen w-full font-inter">
     <Sidebar user={loggedIn}/>
+    <div className="flex size-full flex-col">
+    <div className="root-layout">
+    <Image src="/icons/logo.svg" width={30} height={30} alt="logo"/>
+    <div>
+      <MobileNav user={loggedIn}/>
+    </div>
+    </div>
     {children}
+    </div>
  </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from "@/components/ui/HeaderBox";
+import RightSidebar from "@/components/ui/RightSidebar";
 import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
 
 const Home = () => {
-    const loggedIn = { firstName: 'RAJ' };
+    const loggedIn = { firstName: 'RAJ', lastName: 'DEEP', email: 'contactraj@gmail.com' };
 return (
     <section className="home">
     <div className="home-content">
@@ -11,7 +12,9 @@ return (
     subtext="Access and manage your account and transactions efficiently"/>
     <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={12500.350} />
     </header>
+    RECENT TRANSACTIONS
     </div>
+    <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 12350.350}, {currentBalance: 500.760}]}/>
     </section>
 )
 }
